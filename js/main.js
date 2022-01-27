a277caf770e21655b40c2867f785e263ed5c0441
Kees den Hollander <cornelisgijsbert@gmail.com>
2022-01-27 21:12:42 +0100

    add main.js for validate emailadress

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function validateEmail(inputText) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var x = document.getElementById("message");
+    if (inputText.value.match(mailformat)) {
+        //alert("Valid email address!");
+        x.style.display = "none";
+        document.form1.text1.focus();
+        return true;
+    } else {
+        x.style.display = "block";
+        //alert("You have entered an invalid email address!");
+        document.form1.text1.focus();
+        return false;
+    }
+}
