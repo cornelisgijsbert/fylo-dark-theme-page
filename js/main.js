fa919cdf8eeaa5973ed46bef5b72f6811b30adcf
Kees den Hollander <cornelisgijsbert@gmail.com>
2022-02-11 23:24:44 +0100

    try to fix js, page is scrolling to the top when clicking de email button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 function validateEmail(inputText) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    var x = document.getElementById("message");
+    const x = document.getElementById("message");
     if (inputText.value.match(mailformat)) {
         //alert("Valid email address!");
+        //x.style.display = "none";
+        x.innerHTML = "";
         document.form1.text1.focus();
-        x.style.display = "none";
         return true;
+
     } else {
         //alert("You have entered an invalid email address!");
+        //x.style.display = "block";
+        x.innerHTML = "Please enter a valid email adress";
         document.form1.text1.focus();
-        x.style.display = "block";
         return false;
+
     }
 }
