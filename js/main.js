932c9207b040516f6babb05845e609816188cca2
Kees den Hollander <cornelisgijsbert@gmail.com>
2022-02-13 21:31:09 +0100

    email validation: krijg in console.log geen waarde van var txt

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-document.getElementById("submit").addEventListener("click", validateEmail);
+document.getElementById("button1").addEventListener("click", validateEmail);
 var x = document.getElementById("message");
-var txt = document.forms["form1"]["text1"].value;
+//var txt = document.forms["form1"]["text1"].value;
+var txt = document.getElementById("text1").value;
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-function validateEmail () {
-    if (txt.match(mailformat)) {
-      x.innerText = "";  
+function validateEmail() {
+    if (txt === mailformat) {
+        x.innerText = "";
+        return true;
     } else {
-      x.innerText = "Enter a valid email adress";
+        x.innerText = "Enter a valid email adress";
+        return false;
     }
 }
+console.log(txt);
 
 /*function validateEmail(inputText) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
